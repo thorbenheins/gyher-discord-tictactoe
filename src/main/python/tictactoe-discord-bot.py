54cda44466e6fe488da11bb6544e240fd146964f
Thorben Heins <mail@thorben-heins.de>
2024-01-25 10:53:45 +0100

    Some refactoring

--- src/main/python/tictactoe-discord-bot.py
+++ src/main/python/tictactoe-discord-bot.py
@@ -1,13 +1,16 @@
 import discord
 import os
 
-class MyClient(discord.Client):
+
+class TicTacToeBotClient(discord.Client):
+    message_count: int = 0
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-
     async def on_message(self, message: discord.Message):
-        print(f'Message from {message.author}: {message.content}')
+        self.message_count = self.message_count + 1
+        print(f'Message #{self.message_count} from {message.author}: {message.content}')
 
         if message.author == client.user:
             return
@@ -15,8 +18,9 @@ class MyClient(discord.Client):
         if message.content.startswith('Hallo'):
             await message.channel.send('Hello ' + message.author.name + '!')
 
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = TicTacToeBotClient(intents=intents)
 client.run(os.environ['BOT_TOKEN'])
